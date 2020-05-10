6d549aec2e9debed90b7bae77dfea39c9f5792d9
Ryan Govostes <rgovostes+git@gmail.com>
2020-05-10 17:49:17 -0400

    Add basic Java publisher

--- java/redis-pub/src/main/java/edu/whoi/redispub/App.java
+++ java/redis-pub/src/main/java/edu/whoi/redispub/App.java
@@ -1,5 +1,12 @@
 package edu.whoi.redispub;
 
+import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
+import java.util.Random;
+import org.json.JSONObject;
+import redis.clients.jedis.Jedis;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +15,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Jedis jedis = new Jedis(args[0]);
+     
+        Random r = new Random();
+
+        while (true) {
+            JSONObject obj = new JSONObject();
+            obj.put("time", ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME));
+
+            String channel = "/updates/" + r.nextInt(5);
+            System.out.println("Publishing to channel " + channel);
+
+            jedis.publish(channel, obj.toString());
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
     }
 }
